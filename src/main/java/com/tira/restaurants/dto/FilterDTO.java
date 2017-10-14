cd4d792155cc691f0b60c01075d0a2d3e8b0ec49
Tyrx95 <amartiric@gmail.com>
2017-10-14 22:46:13 +0200

    added filterDTO, previously unversioned

---
+++ src/main/java/com/tira/restaurants/dto/FilterDTO.java
@@ -0,0 +1,30 @@
+package com.tira.restaurants.dto;
+
+public class FilterDTO {
+
+	private Integer itemsPerPage;
+	private Integer pageNumber;
+	private String searchText;
+	
+	public Integer getItemsPerPage() {
+		return itemsPerPage;
+	}
+	public void setItemsPerPage(Integer itemsPerPage) {
+		this.itemsPerPage = itemsPerPage;
+	}
+	public Integer getPageNumber() {
+		return pageNumber;
+	}
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+	public String getSearchText() {
+		return searchText;
+	}
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+	
+	
+	
+}
